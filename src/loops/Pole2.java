1a658cfa8b93c7d8fb177429dd6c92ae910ae9a1
khoi.hoang <filiphoangvn@gmail.com>
2023-10-31 08:41:57 +0100

    Pole advanced

--- src/loops/Pole2.java
+++ src/loops/Pole2.java
@@ -2,6 +2,34 @@ package loops;
 
 public class Pole2 {
     public static void main(String[] args) {
-        double[] doubleArray = {15.2, 123.1};
+        double[] doubleArray = {15.2, 123.1, 4.2, 6.9, 94.2, 7894.1};
+        for (int i = 0; i < doubleArray.length; i++) {
+            System.out.println(doubleArray[i]);
+
+        }
+        for (int i = 0; i < doubleArray.length; i++) {
+            if (doubleArray[i] < 0){
+                System.out.println(doubleArray[i]);
+               }
+            }
+                double arraySum = 0;
+        for (int i = 0; i < doubleArray.length; i++) {
+            arraySum = arraySum + doubleArray[i];
+
+        }
+        System.out.println("Sum of array: " + arraySum);
+        System.out.println("Average value: " + (arraySum / doubleArray.length)); //prumer v poli
+
+
+
+        double[] points = {-2,-1,0,1,2};
+        for (int i = 0; i < points.length; i++) {
+            System.out.println("body: ");
+            System.out.println("x = " + points[i]);
+            double y = 2*points[i] +5;
+            System.out.println("y = " + y);
+
+        }
+        }
     }
-}
+
