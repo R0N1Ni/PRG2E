459fdabb570559a8b866455943d4fc684d1f9f91
khoi.hoang <filiphoangvn@gmail.com>
2023-11-09 12:20:02 +0100

    priprava na test

---
+++ src/loops/Random.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class Random {
+    public static void main(String[] args) {
+        //random cisla (-50, 50)
+        int randomNegative = (int)(Math.random()*101-50);
+        System.out.println(randomNegative);
+    }
+}
