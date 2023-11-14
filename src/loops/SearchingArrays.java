ea6b88c899c5fa8a8b6e0e3ca224dcbb520f4f8f
khoi.hoang <filiphoangvn@gmail.com>
2023-11-14 08:45:20 +0100

    Pole advanced

--- src/loops/SearchingArrays.java
+++ src/loops/SearchingArrays.java
@@ -32,5 +32,23 @@ public class SearchingArrays {
         System.out.println(Arrays.toString(emptyArray));
 
 
+        int [] switches = {5,6,9};
+        int temp = switches[0];
+        switches [0] = switches [2];
+        switches[2] = temp;
+
+        int [] pole ={15,9847,651,654,65,136,1321} ;
+        for (int i = 0; i < pole.length - 1; i++) {
+            if (pole[i] > pole [i+1]){
+                System.out.println("Neni serazeno");
+                break;
+            }
+
+        }
+//        int [] array = new int[10];
+//        while (){
+//            System.out.println("Zadej cisla od 1-10");
+//            if ();
+//        }
     }
 }
