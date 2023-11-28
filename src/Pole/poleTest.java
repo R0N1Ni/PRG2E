6dee2383a919d26f4d19e141b741536e10fc3c19
khoi.hoang <filiphoangvn@gmail.com>
2023-11-28 08:04:54 +0100

    Pole priprava na test

--- src/Pole/poleTest.java
+++ src/Pole/poleTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class poleTest {
     public static void main(String[] args) {
        //2. ukol
-        int [] arr = {0,1,2,4,3,5,6};
+        int [] arr = {0,1,2,4,3,5,6,7};
 //        int even = 0;
 //        int odd = 0;
 //        for (int i = 0; i < arr.length; i++) {
