7b07eb54d5c9d9a7a6d03a238ef006cd1feedfca
khoi.hoang <filiphoangvn@gmail.com>
2024-01-18 12:20:30 +0100

    dvojty pole

---
+++ src/Pole/MultipleArrays.java
@@ -0,0 +1,14 @@
+package Pole;
+
+public class MultipleArrays {
+    public static void main(String[] args) {
+        int [] array = {2,5,3,6,8,4,5};
+        int [][] moreArrays = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        int [][] coordinates = {{2,3}, {4,5}, {9,6}, {2,5}};
+
+        System.out.println(moreArrays[1][2]);
+
+
+    }
+}
