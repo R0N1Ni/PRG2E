05cfbf1919bfd36bcdd42350a25d8065f11dac14
khoi.hoang <filiphoangvn@gmail.com>
2024-01-23 08:41:39 +0100

    pole v poli

--- src/Pole/MultipleArrays.java
+++ src/Pole/MultipleArrays.java
@@ -1,5 +1,7 @@
 package Pole;
 
+import java.util.Arrays;
+
 public class MultipleArrays {
     public static void main(String[] args) {
         int [] array = {2,5,3,6,8,4,5};
@@ -9,6 +11,36 @@ public class MultipleArrays {
 
         System.out.println(moreArrays[1][2]);
 
+        //vygenerovani pole
+
+        int rows = 10;
+        int cols = 10;
+
+        int [][] sequence = new int[10][10];
+        int counter = 1;
+        for (int i = 0; i < sequence.length; i++) {
+            for (int j = 0; j < sequence[i].length; j++) {
+             sequence[i][j] = counter;
+             counter++;
+
+            }
+        }
+
+        //vypsani pole
+        for (int i = 0; i < sequence.length; i++) {
+            for (int j = 0; j < sequence[i].length; j++) {
+                System.out.print(sequence[i][j] + " \t ");
+            }
+            System.out.println();//odradkovani
+        }
+
+        //vypsani pole pozpatku
+        for (int i = sequence.length - 1; i >= 0; i--) {
+            for (int j = sequence[i].length - 1; j >= 0; j--) {
+                System.out.print(sequence[i][j] + " \t ");
+            }
+            System.out.println();//odradkovani
+        }
 
     }
 }
