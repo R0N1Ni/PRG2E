b9c085a4c3fad2394203c4dfca727f90c73f544e
khoi.hoang <filiphoangvn@gmail.com>
2024-03-12 08:45:23 +0100

    metody

---
+++ src/metody/PredTestem.java
@@ -0,0 +1,27 @@
+package metody;
+
+public class PredTestem {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int dayCount(int day, int month) {
+        return 0;
+    }
+    public static boolean isLeapYear (int year){
+        return year % 4 == 0 && year % 100 !=0;
+    }
+    public static void textAnalysis(String text){
+        // void kdyz se nic nevraci
+    }
+ public static boolean isTriangle (int a, int b, int c){
+        return true;
+ }
+ public static double triangleArea(int a, int b, int c){
+        return -1.0;
+ }
+ public static int digitSum (int num){
+        return 1;
+ }
+}
---
+++ src/metody/Recursion.java
@@ -0,0 +1,21 @@
+package metody;
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(notRecursive(2,5));
+        System.out.println(Recursive(2,5));
+    }
+    public static int notRecursive(int root, int power){
+        int result = 1;
+        for (int i = 0; i < power; i++) {
+            result *= root;
+        }
+        return result;
+    }
+    public static int Recursive(int root, int power){
+        if (power ==1){
+            return root; //jednoduche reseni
+        }
+        return Recursive(root, power - 1) * root;
+    }
+}
