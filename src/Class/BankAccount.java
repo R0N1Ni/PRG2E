27ca22e910660f5750ed4fdbe5fee5937633343f
khoi.hoang <filiphoangvn@gmail.com>
2024-03-26 08:44:24 +0100

    Class-bankovni ucet

---
+++ src/Class/BankAccount.java
@@ -0,0 +1,28 @@
+package Class;
+
+public class BankAccount {
+    long bid = 12346589451654L;
+    int balance;
+    String owner;
+
+    void insert(int amount){
+        balance += amount;
+    }
+    void withdrawl(int amount){
+        if (balance < amount){
+            System.out.println("Chuda kundo");
+        }else {
+            balance -= amount;
+        }
+    }
+
+    void transaction(int amount, BankAccount reciever){
+        if (balance > amount){
+            balance -= amount;
+            reciever.balance += amount;
+        } else {
+            System.out.println("Prevod se nezdaril, jses chuda kunda");
+        }
+
+    }
+}
---
+++ src/Class/Banking.java
@@ -0,0 +1,24 @@
+package Class;
+
+public class Banking {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount();
+        acc1.balance = 1000;
+        acc1.bid = 123456;
+        acc1.owner = "Milan";
+
+        acc1.insert( 1000);
+        System.out.println(acc1.balance);
+        acc1.withdrawl(500);
+        System.out.println(acc1.balance);
+        acc1.withdrawl(50000);
+
+        BankAccount acc2 = new BankAccount();
+        acc2.balance = 500;
+        System.out.println("ACC1: " + acc1.balance);
+        System.out.println("ACC2: " + acc2.balance);
+        acc1.transaction(800, acc2);
+        System.out.println("ACC1: " + acc1.balance);
+        System.out.println("ACC2: " + acc2.balance);
+    }
+}
