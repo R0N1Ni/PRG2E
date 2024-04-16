b81ce44381dbc32d6264b5258f20981e586d3225
khoi.hoang <filiphoangvn@gmail.com>
2024-04-16 08:44:02 +0200

    OOP

---
+++ src/OOP/Ticking.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class Ticking {
+    public static void main(String[] args) {
+        Clock basic = new Clock(8,8,8,true);
+
+        while (basic.hours <= 3){
+            basic.tick();
+            basic.printTime();
+        }
+        Clock dalsi = new Clock(16,30,0,true);
+        System.out.println("Casy: ");
+        dalsi.printTime();
+        basic.printTime();
+        System.out.println("Rozdil " + basic.difference(dalsi));
+
+        Clock fixed = new Clock(0,0,0,false);
+        fixed.hours = 10;
+    }
+}
