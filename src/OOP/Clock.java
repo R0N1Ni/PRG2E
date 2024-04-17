134a7a804c5285a200876f8aea95b385375d3e83
Filip <filiphoangvn@gmail.com>
2024-04-17 21:58:43 +0200

    OOP-Special casy a budik

--- src/OOP/Clock.java
+++ src/OOP/Clock.java
@@ -3,12 +3,17 @@ package OOP;
 public class Clock {
     public int hours, minutes, seconds;
     boolean format24;
+    int alarmHour, alarmMin, alarmSec;
 
-    public Clock(int hours, int minutes, int seconds, boolean format24) {
+
+    public Clock(int hours, int minutes, int seconds, boolean format24, int alarmHour, int alarmMin, int alarmSec) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
         this.format24 = format24;
+        this.alarmHour = alarmHour;
+        this.alarmMin = alarmMin;
+        this.alarmSec = alarmSec;
 
     }
     int difference (Clock other){
@@ -19,7 +24,7 @@ public class Clock {
         return difference;
     }
     void printTime (){
-        System.out.println(hours + " " + minutes + " " + seconds);
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
     void tick (){
         seconds++;
@@ -41,7 +46,43 @@ public class Clock {
                 hours = 1;
 
             }
+        }
 
+       if (isAlarm()){
+           System.out.println("Budicek zmrde");
+           printTime();
+           System.out.println("Budicek zmrde");
+       }
+
+
+//        if (specialTimes()){
+//            printTime();
+//        }
+
+        boolean isSpecial = specialTimes();
+        if (isSpecial ){
+            printTime();
+        }
+    }
+    boolean specialTimes(){
+        boolean sameDigit = hours == minutes && hours == seconds;
+        boolean specialMinutes = minutes == 15 || minutes == 30 || minutes == 45;
+        boolean zeroSeconds = seconds == 0;
+        if (sameDigit || (specialMinutes && zeroSeconds)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isAlarm(){
+        boolean sameHour = hours == alarmHour;
+        boolean sameMin = minutes == alarmMin;
+        boolean sameSec = seconds == alarmSec;
+        if (sameHour && sameSec && sameMin){
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- src/OOP/Ticking.java
+++ src/OOP/Ticking.java
@@ -2,19 +2,23 @@ package OOP;
 
 public class Ticking {
     public static void main(String[] args) {
-        Clock basic = new Clock(8,8,8,true);
+        Clock basic = new Clock(1,0,0,true,5,24,16);
 
-        while (basic.hours <= 3){
+        while (basic.hours <= 6){
             basic.tick();
-            basic.printTime();
-        }
-        Clock dalsi = new Clock(16,30,0,true);
-        System.out.println("Casy: ");
-        dalsi.printTime();
-        basic.printTime();
-        System.out.println("Rozdil " + basic.difference(dalsi));
 
-        Clock fixed = new Clock(0,0,0,false);
-        fixed.hours = 10;
+//            basic.printTime();
+        }
+//        Clock dalsi = new Clock(16,30,0,true);
+//        System.out.println("Casy: ");
+//        dalsi.printTime();
+//        basic.printTime();
+//        System.out.println("Rozdil " + basic.difference(dalsi));
+
+//        Clock fixed = new Clock(0,0,0,false);
+//        fixed.hours = 10;
+
+
+
     }
 }
