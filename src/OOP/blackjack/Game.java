1856128c5774fa9a77247d865a0fae91fbb22428
khoi.hoang <filiphoangvn@gmail.com>
2024-05-21 08:43:55 +0200

    OOP-blackjack

--- src/OOP/blackjack/Game.java
+++ src/OOP/blackjack/Game.java
@@ -11,9 +11,47 @@ public class Game {
         this.player = player;
         this.dealerValue = (int)(Math.random()* 11 + 10);
     }
+
+    boolean takeTurn(){
+        System.out.println("Liznout si dalsi kartu?");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine().equals("ano");
+    }
+
+    void printInfo(){
+        System.out.println("===============");
+        System.out.println("Hrac:\t " + player.getHandValue());
+        System.out.println("Dealer:\t " + dealerValue);
+        System.out.println("===============");
+    }
     void start(){
         if (wantToPlay()){
+            dealerValue = (int)(Math.random()* 11 + 10);
             bet = 2 * betAmount();
+            System.out.println("Hra zacala a vyherni castka je " + bet);
+            System.out.println("dealer ma " + dealerValue);
+
+            player.pickCard();
+
+            while(takeTurn() && player.getHandValue() < 21){
+                player.pickCard();
+                System.out.println("V ruce mas " + player.getHandValue());
+            }
+                if (player.getHandValue() == 21){
+                    System.out.println("Hrac " + player.name + " vyhral " + bet);
+                    player.money += bet;
+                }
+                if (player.getHandValue() == dealerValue){
+                    player.money += bet/2;
+                } else if (player.getHandValue() > 21){
+                    System.out.println("Hrac " + player.name + " prohrava");
+                }else if (21 - player.getHandValue()> 21 - dealerValue) {
+                    System.out.println("Hrac " + player.name + " prohrava");
+                } else {
+                    System.out.println("Hrac " + player.name + " vyhral " + bet);
+                    player.money += bet;
+                }
+            System.out.println("Hra ukoncena ");
 
         } else {
             System.out.println("Srabe");
@@ -44,5 +82,8 @@ public class Game {
     public static void main(String[] args) {
         Player max = new Player("Max", 1000);
         Game blackjack = new Game(max);
+
+        blackjack.start();
+
     }
 }
